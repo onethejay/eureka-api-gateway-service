7c9ffaec1a34acda3b1c6e43946988489f30f5a6
onethejay <jhjang951@gmail.com>
2022-04-11 22:44:50 +0900

    Initial commit

---
+++ src/main/java/com/tistory/onethejay/apigatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,48 @@
+package com.tistory.onethejay.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Custom Pre Filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();          //reactive
+            ServerHttpResponse response = exchange.getResponse();       //reactive
+
+            log.info("Global Filter baseMessage: {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                log.info("Global Filter Start: request id -> {}", request.getId());
+            }
+
+            // Custom Post filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> { //Spring5 WebFlux - Mono
+                if (config.isPostLogger()) {
+                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
+                }
+            }));
+        };
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
+
